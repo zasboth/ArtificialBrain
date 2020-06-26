f59e0e006d12162f285666ca6eb90d4555d90b8e
Zsolt Asboth <zsolt.asboth@outlook.com>
2020-06-26 05:51:32 +0200

    Some environmetal changes

--- CppPrototype/AbstarctContainer_test.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * AbstarctContainer_test.cpp
- *
- *  Created on: 2020. jún. 11.
- *      Author: zsolt
- */
-
-#include <AbstarctContainer.h>
-
---
+++ CppPrototype/test/AbstarctContainer_test.cpp
@@ -0,0 +1,10 @@
+/*
+ * AbstarctContainer_test.cpp
+ *
+ *  Created on: 2020. j�n. 11.
+ *      Author: zsolt
+ */
+
+#include <AbstractContainer.h>
+#include <gtest/gtest.h>
+
