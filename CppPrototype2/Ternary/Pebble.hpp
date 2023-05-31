3a174cbf4ce8efdd073a0e81e2daf3a8450cc27a
zsola <zsolt.asboth@gmail.com>
2023-05-31 18:14:57 +0200

    Perceptron template increased

---
+++ CppPrototype2/Ternary/Pebble.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <bitset>
+#include <vector>
+#include "TernaryBit.hpp"
+
+template <size_t Size>
+class Pebble
+{
+public:
+    Pebble();
+    ~Pebble(){};
+
+    
+private:
+    size_t length = Size;
+    TernaryBit[Size] bits;
+};
+
+template <size_t Size>
+inline Pebble<Size>::Pebble()
+{
+    for (size_t i = 0; i < Size; i++)
+    {
+        bits[i] = TernaryBit();
+    }
+}
+
